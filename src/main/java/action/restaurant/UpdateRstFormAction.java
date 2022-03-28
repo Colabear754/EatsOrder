aa600397917823ca6da703ed90d4e4a4bcacc21f
Colabear754 <jky2406@gmail.com>
2022-03-28 09:11:03 +0900

    오류 수정

--- src/main/java/action/restaurant/UpdateRstFormAction.java
+++ src/main/java/action/restaurant/UpdateRstFormAction.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import action.CommandAction;
+import restaurant.RestaurantDAO;
 import restaurant.RestaurantDTO;
 
 public class UpdateRstFormAction implements CommandAction {
@@ -13,26 +14,9 @@ public class UpdateRstFormAction implements CommandAction {
 		// TODO Auto-generated method stub
 		request.setCharacterEncoding("utf-8");
 		int rst_id = Integer.parseInt(request.getParameter("rst_id"));
-		int category_id = Integer.parseInt(request.getParameter("category_id"));
-		String rst_name = request.getParameter("rst_name");
-		String phone = request.getParameter("phone");
-		String address = request.getParameter("address");
-		int min_order = Integer.parseInt(request.getParameter("min_order"));
-		String origin = request.getParameter("origin");
-		String hours = request.getParameter("hours");
-		String bussiness_number = request.getParameter("bussiness_number");
-		String bussiness_name = request.getParameter("bussiness_name");
-		String payment = request.getParameter("payment");
-		int delivery_tip = Integer.parseInt(request.getParameter("delivery_tip"));
-		String rst_notice = request.getParameter("rst_notice");
-		String estimated_time = request.getParameter("estimated_time");
-		String rst_photo = request.getParameter("rst_photo");
-		String rst_logo = request.getParameter("rst_logo");
-		int enable = Integer.parseInt(request.getParameter("enable"));
+		RestaurantDAO rstProcess = new RestaurantDAO();
 
-		RestaurantDTO restaurant = new RestaurantDTO(rst_id, category_id, rst_name, phone, address, min_order, origin, hours,
-				bussiness_number, bussiness_name, payment, delivery_tip, rst_notice, estimated_time, rst_photo, rst_logo,
-				enable);
+		RestaurantDTO restaurant = rstProcess.getRestaurant(rst_id);
 		
 		request.setAttribute("restaurant", restaurant);
 
