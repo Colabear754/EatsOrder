857ea85fa8a8c9e0e9be5f365a3b22f6fa38afe2
kyj <jky2406@gmail.com>
2022-04-10 22:33:27 +0900

    코멘트 수정

--- src/main/webapp/member/js/update_member_script.js
+++ src/main/webapp/member/js/update_member_script.js
@@ -2,6 +2,10 @@
 작성자: 김나연
 작성완료일: 22/04/04
 페이지명: 회원정보수정
+
+수정자 : 정건영
+수정일 : 2022/04/10
+수정내용 : 휴대폰번호 관련 부분 수정
 */
 
 $(document).ready(function() { //제이쿼리 정규식 표현
@@ -35,62 +39,62 @@ $(document).ready(function() { //제이쿼리 정규식 표현
     });
     
     $("form").submit(function(){
-    //새 비밀번호 공백 확인
-    if($("#newPassword").val() == ""){
-	    alert("비밀번호를 입력해주세요");
-	    $("#newPassword").focus();
-	    return false;
-    }
-    
-    //새 비밀번호 유효성검사
-    if(!passwdCheck.test($("#newPassword").val())) {
-	    alert("형식에 맞게 비밀번호를 입력해주세요");
-	    $("#newPassword").val("");
-	    $("#newPassword").focus();
-	    return false;
-    }
- 
-    //새 비밀번호, 새 비밀번호 확인 똑같은지
-     if($("#newPassword").val() != ($("#renew_pwd").val())){ 
-	     alert("비밀번호가 틀립니다.");
-	     $("#newPassword").val("");
-	     $("#renew_pwd").val("");
-	     $("#newPassword").focus();
-	     return false;
-     }
-
-    //닉네임 공백 확인
-    if($("#name").val() == ""){
-	    alert("닉네임을 입력해주세요");
-	    $("#name").focus();
-	    return false;
-    }
-
-    //닉네임 유효성 검사
-    if(!nickNameCheck.test($("#name").val())){
-    	alert("형식에 맞게 입력해주세요");
-    	$("#name").val("");
-    	$("#name").focus();
-    	return false;
-    }
-
-    //휴대폰 공백 확인
-    if($("#phone").val() == ""){
-    	alert("휴대폰 번호를 입력해주세요");
-    	$("#phone").focus();
-    	return false;
-    }
-
-    //휴대폰 유효성 검사
-    if(!phoneCheck.test($("#phone").val())){
-	    alert("형식에 맞게 입력해주세요");
-	    $("#phone").val("");
-	    $("#phone").focus();
-	    return false;
-    }
-    
-    $(document).on("keyup", ".phoneNumber", function() { 
-    	$(this).val($(this).val().replace(/[^0-9]/g, "").replace(/(^02|^0505|^1[0-9]{3}|^0[0-9]{2})([0-9]+)?([0-9]{4})$/,"$1-$2-$3").replace("--", "-") ); 
+	    //새 비밀번호 공백 확인
+	    if($("#newPassword").val() == ""){
+		    alert("비밀번호를 입력해주세요");
+		    $("#newPassword").focus();
+		    return false;
+	    }
+	    
+	    //새 비밀번호 유효성검사
+	    if(!passwdCheck.test($("#newPassword").val())) {
+		    alert("형식에 맞게 비밀번호를 입력해주세요");
+		    $("#newPassword").val("");
+		    $("#newPassword").focus();
+		    return false;
+	    }
+	 
+	    //새 비밀번호, 새 비밀번호 확인 똑같은지
+	     if($("#newPassword").val() != ($("#renew_pwd").val())){ 
+		     alert("비밀번호가 틀립니다.");
+		     $("#newPassword").val("");
+		     $("#renew_pwd").val("");
+		     $("#newPassword").focus();
+		     return false;
+	     }
+	
+	    //닉네임 공백 확인
+	    if($("#name").val() == ""){
+		    alert("닉네임을 입력해주세요");
+		    $("#name").focus();
+		    return false;
+	    }
+	
+	    //닉네임 유효성 검사
+	    if(!nickNameCheck.test($("#name").val())){
+	    	alert("형식에 맞게 입력해주세요");
+	    	$("#name").val("");
+	    	$("#name").focus();
+	    	return false;
+	    }
+	
+	    //휴대폰 공백 확인
+	    if($("#phone").val() == ""){
+	    	alert("휴대폰 번호를 입력해주세요");
+	    	$("#phone").focus();
+	    	return false;
+	    }
+	
+	    //휴대폰 유효성 검사
+	    if(!phoneCheck.test($("#phone").val())){
+		    alert("형식에 맞게 입력해주세요");
+		    $("#phone").val("");
+		    $("#phone").focus();
+		    return false;
+	    }
     });
-  });
+    
+    $('#phone').keyup(function() {
+    	$(this).val( $(this).val().replace(/[^0-9]/g, "").replace(/(^02|^0505|^1[0-9]{3}|^0[0-9]{2})([0-9]+)?([0-9]{4})/, "$1-$2-$3").replace("--", "-") );
+    })
 });
