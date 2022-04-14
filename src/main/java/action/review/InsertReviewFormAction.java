73ba969b8065b3b3310a815286d2b5cb238ec890
woolim <liamh0218@gmail.com>
2022-04-15 00:34:53 +0900

    파일 여러개 업로드하기위해 필요한 자바구문 추가

--- src/main/java/action/review/InsertReviewFormAction.java
+++ src/main/java/action/review/InsertReviewFormAction.java
@@ -7,8 +7,32 @@ import action.CommandAction;
 
 public class InsertReviewFormAction implements CommandAction {
 
+	//추가. 작성자:허우림. 작성일자:22-04-14. 내용:여러 개의 파일 업로드
+	
+	//request,paramName을 인자로 전달받아서 현재의 폼 요소들 중 paramName으로 지정된 부분에 입력된 값을 보존하고 현재의 폼을 전송하는 메소드
+	public String getParam(HttpServletRequest request,String paramName) {
+		//이 페이지가 처음 실행될때 request.getParameter()로 받아오는 값이 없기 때문에 이부분이 null인지 아닌지를 체크
+		if(request.getParameter(paramName)!=null){
+			//if조건이 null이 아닐때(이 페이지를 전송했을때), 파라미터(폼 요소들)을 가져와서 결과값으로 반환
+			return request.getParameter(paramName);
+		}else {
+			return "";
+		}
+	}
 	@Override
 	public String requestProcess(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		
+		request.setCharacterEncoding("UTF-8");
+		int filecounter=0;
+		//InsertReviewForm.jsp의 addcnt(업로드할 파일수)를 입력했다면,그것을 filecounter에 담음
+		if(request.getParameter("addcnt")!=null) {
+			filecounter=Integer.parseInt(request.getParameter("addcnt"));
+		}
+		
+		//filecounter변수를 InsertReviewForm.jsp에서 for문 출력에 사용하기 위해 저장.
+		//request.setAttribute("filecounter",filecounter);
+	//-----------------------
+		
 		// 리뷰 작성 폼 액션클래스
 		String order_number = request.getParameter("order_number");
 		String email = request.getParameter("email");
