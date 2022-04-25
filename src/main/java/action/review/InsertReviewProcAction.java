b0b983cd2eab5d27c865b1c30398126acf095db0
Colabear754 <jky2406@gmail.com>
2022-04-25 22:25:51 +0900

    파일 저장경로 상대경로로 수정

--- src/main/java/action/review/InsertReviewProcAction.java
+++ src/main/java/action/review/InsertReviewProcAction.java
@@ -17,50 +17,55 @@ public class InsertReviewProcAction implements CommandAction {
 
 	//이미지의 실제 파일이름을 테이블에 저장하기위해 statice 변수 선언
 	static String filename;
+
 	@Override
 	public String requestProcess(HttpServletRequest request, HttpServletResponse response) throws Throwable {
-		
+
 		//추가) 작성자:허우림. 작성일:22-04-17------
 		//사진 첨부
 		//로드 파일의 실제경로
-		String saveFolder= "c:/webtest/4.jsp/2.back-end/sou2/EatsOrder/src/main/webapp/reviewPhoto";
-		String encType="UTF-8";
-		int maxSize=10*1024*1024;
 		
-		ServletContext context=request.getSession().getServletContext();
-		ArrayList saveFiles=new ArrayList();
-		
-		try{
-			MultipartRequest multi=new MultipartRequest(request,saveFolder,maxSize,encType,new DefaultFileRenamePolicy());
-			
-			Enumeration files=multi.getFileNames();
-			
+		// 파일 경로 상대경로로 수정함
+		String saveFolder = this.getClass().getResource("/").getPath();
+		saveFolder = saveFolder.replaceAll("%20", " ");
+		saveFolder = saveFolder.replace("WEB-INF/classes/", "reviewPhoto");
+		String encType = "UTF-8";
+		int maxSize = 10 * 1024 * 1024;
+
+		ArrayList<String> saveFiles = new ArrayList<>();
+
+		try {
+			MultipartRequest multi = new MultipartRequest(request, saveFolder, maxSize, encType,
+					new DefaultFileRenamePolicy());
+
+			Enumeration files = multi.getFileNames();
+
 			//hasMoreElements()는 Enumeration에 요소가 있는지 없는지 체크. 하나가 있으면 그 다음 요소가 있는지 없는지 체크.
-			while(files.hasMoreElements()){
-				String name=(String)files.nextElement();
-				InsertReviewProcAction.filename=multi.getFilesystemName(name);
+			while (files.hasMoreElements()) {
+				String name = (String) files.nextElement();
+				InsertReviewProcAction.filename = multi.getFilesystemName(name);
 				saveFiles.add(multi.getFilesystemName(name));
 			}
-			
+
 			String email = multi.getParameter("email");
 			String order_number = multi.getParameter("order_number");
 			String content = multi.getParameter("content");
 			int rating = Integer.parseInt(multi.getParameter("rating"));
 			ReviewDAO reviewProcess = new ReviewDAO();
-			
+
 			String[] photos = new String[5];
-			int photoCount = 0;	// 사진 개수
-			
+			int photoCount = 0; // 사진 개수
+
 			//저장된 사진의 파일명을 photos 배열에 형변환 하여 담음
-			for(int i=0; i<saveFiles.size(); i++) {
-				photos[i]=(String)saveFiles.get(i);
+			for (int i = 0; i < saveFiles.size(); i++) {
+				photos[i] = (String) saveFiles.get(i);
 			}
-			
-			for (String photo : photos) {	// 사진 개수 계산
+
+			for (String photo : photos) { // 사진 개수 계산
 				photoCount = photo != null ? photoCount + 1 : photoCount;
 			}
-			
-			switch (photoCount) {	// 사진 개수에 따라 호출하는 메소드가 달라짐
+
+			switch (photoCount) { // 사진 개수에 따라 호출하는 메소드가 달라짐
 			case 0:
 				reviewProcess.insertReivew(email, order_number, content, rating);
 				break;
@@ -77,61 +82,47 @@ public class InsertReviewProcAction implements CommandAction {
 				reviewProcess.insertReivew(email, order_number, content, photos[0], photos[1], photos[2], photos[3], rating);
 				break;
 			case 5:
-				reviewProcess.insertReivew(email, order_number, content, photos[0], photos[1], photos[2], photos[3], photos[4], rating);
+				reviewProcess.insertReivew(email, order_number, content, photos[0], photos[1], photos[2], photos[3],
+						photos[4], rating);
 				break;
 			default:
 				break;
 			}
-			
-		}catch(IOException ioe){
+
+		} catch (IOException ioe) {
 			System.out.println(ioe);
-		}catch(Exception ex){
+		} catch (Exception ex) {
 			System.out.println(ex);
 		}
 		//---------------------
-		
+
 		/*
-		// 리뷰 작성 액션클래스
-		request.setCharacterEncoding("utf-8");
-		String email = request.getParameter("email");
-		String order_number = request.getParameter("order_number");
-		String content = request.getParameter("content");
-		int rating = Integer.parseInt(request.getParameter("rating"));
-		ReviewDAO reviewProcess = new ReviewDAO();
-		String[] photos = new String[5];
-		int photoCount = 0;	// 사진 개수
-		
-		for (int i = 0; i < photos.length; i++) {
-			photos[i] = request.getParameter("photo" + (i + 1));
-		}
-		
-		for (String photo : photos) {	// 사진 개수 계산
-			photoCount = photo != null ? photoCount + 1 : photoCount;
-		}
-		
-		switch (photoCount) {	// 사진 개수에 따라 호출하는 메소드가 달라짐
-		case 0:
-			reviewProcess.insertReivew(email, order_number, content, rating);
-			break;
-		case 1:
-			reviewProcess.insertReivew(email, order_number, content, photos[0], rating);
-			break;
-		case 2:
-			reviewProcess.insertReivew(email, order_number, content, photos[0], photos[1], rating);
-			break;
-		case 3:
-			reviewProcess.insertReivew(email, order_number, content, photos[0], photos[1], photos[2], rating);
-			break;
-		case 4:
-			reviewProcess.insertReivew(email, order_number, content, photos[0], photos[1], photos[2], photos[3], rating);
-			break;
-		case 5:
-			reviewProcess.insertReivew(email, order_number, content, photos[0], photos[1], photos[2], photos[3], photos[4], rating);
-			break;
-		default:
-			break;
-		}
-		*/
+		 * // 리뷰 작성 액션클래스 request.setCharacterEncoding("utf-8"); String email =
+		 * request.getParameter("email"); String order_number =
+		 * request.getParameter("order_number"); String content =
+		 * request.getParameter("content"); int rating =
+		 * Integer.parseInt(request.getParameter("rating")); ReviewDAO reviewProcess =
+		 * new ReviewDAO(); String[] photos = new String[5]; int photoCount = 0; // 사진
+		 * 개수
+		 * 
+		 * for (int i = 0; i < photos.length; i++) { photos[i] =
+		 * request.getParameter("photo" + (i + 1)); }
+		 * 
+		 * for (String photo : photos) { // 사진 개수 계산 photoCount = photo != null ?
+		 * photoCount + 1 : photoCount; }
+		 * 
+		 * switch (photoCount) { // 사진 개수에 따라 호출하는 메소드가 달라짐 case 0:
+		 * reviewProcess.insertReivew(email, order_number, content, rating); break; case
+		 * 1: reviewProcess.insertReivew(email, order_number, content, photos[0],
+		 * rating); break; case 2: reviewProcess.insertReivew(email, order_number,
+		 * content, photos[0], photos[1], rating); break; case 3:
+		 * reviewProcess.insertReivew(email, order_number, content, photos[0],
+		 * photos[1], photos[2], rating); break; case 4:
+		 * reviewProcess.insertReivew(email, order_number, content, photos[0],
+		 * photos[1], photos[2], photos[3], rating); break; case 5:
+		 * reviewProcess.insertReivew(email, order_number, content, photos[0],
+		 * photos[1], photos[2], photos[3], photos[4], rating); break; default: break; }
+		 */
 		return "/review/insertReviewProc.jsp";
 	}
 }
