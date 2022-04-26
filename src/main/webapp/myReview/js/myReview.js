e43af3bed30f80ffd95be79d627879a4b63a1055
grapefruit13 <liam.heo11@gmail.com>
2022-04-26 22:49:16 +0900

    주석수정

--- src/main/webapp/myReview/js/myReview.js
+++ src/main/webapp/myReview/js/myReview.js
@@ -30,7 +30,6 @@ $(document).ready(function(){
 	});
 });
 
-//수정버튼 누르면 해당 폼 hide되도록
 $(document).ready(function(){
 	$("[name='submit_btn']").click(function(){
 		$("[name='review_form']").hide();
@@ -58,4 +57,86 @@ $(document).ready(function(){
 		return false;
 	});
 });
+/*
+$(document).ready(function(){
+	$("[name='photo1']").change(function(){
+		var photo=$("[name='photo1']").val();
+		alert(photo);
+	})
+})
 
+
+//작성버튼 누르면, form을 만들어서 값들 전달하도록
+$(document).ready(function(){
+	$("[name='submit_btn']").click(function(){
+		
+		var form= $("#uploadForm")[0];
+		alert("form:"+form);
+		var formData=new FormData(form);
+		alert("formData"+formData);
+		$.ajax({
+			url:'insertMyReview.do',
+			type:'POST',
+			data:formData,
+			success:function(data){
+				alert(data)
+			},
+			error:function(data){
+				alert(data)
+			},
+			contentType:false,
+			processData:false,
+			cache:false
+		}).done(function(data){
+			callback(data);
+		});
+		
+	    var newForm=$('<form></form>');
+	    newForm.attr('id','review_form');
+	    newForm.attr('method','post');
+	    newForm.attr('action','insertMyReview.do');
+	    newForm.attr('enctype','multipart/form-data');
+	
+	    newForm.append($('<input>',{type:'hidden',name:'email',value:$("[name='emails']").val()}));
+	    newForm.append($('<input>',{type:'hidden',name:'pageNum',value:$("[name='pageNums']").val()}));
+	    newForm.append($('<input>',{type:'hidden',name:'order_number',value:$("[name='order_numbers']").val()}));
+	    newForm.append($('<input>',{type:'hidden',name:'rating',value:$("#ratings").val()}));
+	   	    
+	    newForm.append($('<input>',{type:'hidden',name:'content',value:$("[name='contents']").val()}));
+	    
+	    newForm.appendTo('body');
+	    
+	    newForm.submit();
+	    return false;
+	})	;
+});*/
+
+/*$(document).ready(function(){
+	$("[name='submit_btn']").click(function(){
+		
+	    var newForm=$('<form></form>');
+	    newForm.attr('id','review_form');
+	    newForm.attr('method','post');
+	    newForm.attr('action','insertMyReview.do');
+	    newForm.attr('enctype','multipart/form-data');
+	
+	    newForm.append($('<input>',{type:'hidden',name:'email',value:$("[name='emails']").val()}));
+	    newForm.append($('<input>',{type:'hidden',name:'pageNum',value:$("[name='pageNums']").val()}));
+	    newForm.append($('<input>',{type:'hidden',name:'order_number',value:$("[name='order_numbers']").val()}));
+	    newForm.append($('<input>',{type:'hidden',name:'rating',value:$("#ratings").val()}));
+	   
+	    newForm.append($('<input>',{type:'file',name:'photo1',style:'display:none',value:$("[name='photo1']").val()}));
+	    newForm.append($('<input>',{type:'file',name:'photo2',style:'display:none',value:$("[name='photo2']").val()}));
+	    newForm.append($('<input>',{type:'file',name:'photo3',style:'display:none',value:$("[name='photo3']").val()}));
+	    newForm.append($('<input>',{type:'file',name:'photo4',style:'display:none',value:$("[name='photo4']").val()}));
+	    newForm.append($('<input>',{type:'file',name:'photo5',style:'display:none',value:$("[name='photo5']").val()}));
+	    
+	    newForm.append($('<input>',{type:'hidden',name:'content',value:$("[name='contents']").val()}));
+	    
+	    newForm.appendTo('body');
+	    
+	    newForm.submit();
+	    return false;
+	})	;
+});
+*/
