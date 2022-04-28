4a19e78b9c4acbd961eb34d00e4c270904a4286f
grapefruit13 <liam.heo11@gmail.com>
2022-04-28 18:27:03 +0900

    내 리뷰 폴더이동 및 경로변경

--- src/main/java/action/member/MyReviewListAction.java
+++ src/main/java/action/member/MyReviewListAction.java
@@ -7,8 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import action.CommandAction;
 import coupon.CouponDAO;
-import member.MemberDAO;
-import member.MemberInfoDTO;
+import member.*;
 import order.OrderBasicInfoDTO;
 import review.*;
 
@@ -17,17 +16,19 @@ public class MyReviewListAction implements CommandAction {
 
 	@Override
 	public String requestProcess(HttpServletRequest request, HttpServletResponse response) throws Throwable {
-		// 내 리뷰 목록 조회 액션클래스
+	// 내 리뷰 목록 조회 액션클래스
 		request.setCharacterEncoding("utf-8");
 		String pageNum = request.getParameter("pageNum");
-		String email = (String) request.getSession().getAttribute("account");
+		String email=request.getParameter("account");
+		//String email = (String) request.getSession().getAttribute("account");
 		MemberDAO memberProcess = new MemberDAO();
 		CouponDAO couponProcess = new CouponDAO();
 		ReviewDAO reviewProcess = new ReviewDAO();
 		ArrayList<ReviewDetailDTO> reviewData = new ArrayList<>();
 		ArrayList<OrderBasicInfoDTO> orderList = reviewProcess.getReviewToWrite(email);
 		long overDate;	// 작성일로부터 경과 일수를 저장하기 위한 변수
-
+		
+		
 		if (pageNum == null) {
 			pageNum = "1";
 		}
@@ -47,7 +48,8 @@ public class MyReviewListAction implements CommandAction {
 
 		MemberInfoDTO member = memberProcess.getMember(email);
 		int couponCount = couponProcess.getCouponCount(email);
-
+		
+		request.setAttribute("email",email);
 		request.setAttribute("member", member);
 		request.setAttribute("couponCount", couponCount);
 		request.setAttribute("reviewData", reviewData);
